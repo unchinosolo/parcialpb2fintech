935a95a0f1c05fc36ac63aa78044846c7655845d
Leandro Abrego <ilovegrubart@gmail.com>
2023-05-10 23:44:13 -0300

    Primeros tests

---
+++ src/ar/unlam/parcialpb2fintech/Fintech.java
@@ -0,0 +1,21 @@
+package ar.unlam.parcialpb2fintech;
+
+public class Fintech {
+	
+	private String nombre;
+
+	public Fintech(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
---
+++ test/ar/unlam/parcialpb2fintech/FintechTest.java
@@ -0,0 +1,89 @@
+package ar.unlam.parcialpb2fintech;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FintechTest {
+	
+	Fintech nuevaApp = new Fintech("Liberty Coins");
+
+	@Test
+	public void queSePuedaCrearUnUsuarioNuevo() 
+	{
+		
+	}
+	
+	@Test
+	public void QueNoSePuedanCrearUsuariosRepetidos() 
+	{
+		
+	}
+	
+	@Test
+	public void queSePuedaDepositarEnPesos() 
+	{
+		
+	}
+	
+	@Test
+	public void QueSePuedaComprarBitcoinsA_200_Pesos() 
+	{
+		
+	}
+	
+	@Test
+	public void QueSePuedaComprarEtherumA_100_Pesos() 
+	{
+		
+	}
+	
+	@Test
+	public void QueSePuedaComprarDaiA_50_Pesos() 
+	{
+		
+	}
+	
+	@Test
+	public void queSePuedaTenerDivididoElSaldoEnPesosYEnCripto() 
+	{
+		
+	}
+	
+	@Test
+	public void QueSePuedaRetirarCriptomonedas() 
+	{
+		
+	}
+	
+	@Test
+	public void QueSePuedaRetirarPesos() 
+	{
+		
+	}
+	
+	@Test
+	public void queSePuedaTransferirDineroAOtroUsuario() 
+	{
+		
+	}
+	
+	@Test
+	public void queSePuedaAgendarUnUsuarioExternoComoAmigo() 
+	{
+		
+	}
+	
+	@Test
+	public void queSeAcrediteUnExtraDe_5_PorcientoDeBitcoinEnUsuariosQueSuperenElAñoDeUso() 
+	{
+		
+	}
+	
+	@Test
+	public void queSePuedaRegistrarLasTransaccionesHechas() 
+	{
+		
+	}
+
+}
